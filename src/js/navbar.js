8c3e641838510f811720838c85ac99101d620483
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2021-04-03 21:02:14 -0300

    add mobile navbar js and styling

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,1 +1,18 @@
-const navbarLinks = document.querySelector('.navbar__links');
+const navbarLink = document.querySelector('.navbar__links');
+const burger = document.querySelector('.burger');
+const navLinksA = document.querySelectorAll('.navbar__links a');
+
+const toggle = () => {
+  burger.addEventListener('click', () => {
+    navbarLink.classList.toggle('navbar__active')
+    navLinksA.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .3}s`;
+      }
+    });
+  })
+}
+
+toggle()

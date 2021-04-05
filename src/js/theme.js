a44575a2cdeeb5e374c4a002d28d878fa453dc05
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2021-04-05 18:25:39 -0300

    update routing, update location.pathname if()

--- src/js/theme.js
+++ src/js/theme.js
@@ -8,6 +8,9 @@ function setTheme(themeName) {
   if (page === '/index.html') {
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
+  } if (page === '/') {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
   } else {
     localStorage.setItem('darktheme', themeName)
     document.documentElement.className = themeName;
@@ -34,19 +37,21 @@ function toggleDarkTheme() {
 
 // Immediately invoked function to set the theme on initial load
 (function () {
-  if (page === 'index.html' && localStorage.getItem('theme') === 'theme-1') {
+  if ((page === '/index.html' || page === '/') && (localStorage.getItem('theme') === 'theme-1')) {
     setTheme('theme-1');
-  } if (page === 'index.html' && localStorage.getItem('theme') === 'theme-2') {
+  } if ((page === '/index.html' || page === '/') && (localStorage.getItem('theme') === 'theme-2')) {
     setTheme('theme-2');
-  } if (page === 'dark.html' && localStorage.getItem('darktheme') === 'dark-theme-1') {
+  } if (page === '/dark.html' && localStorage.getItem('darktheme') === 'dark-theme-1') {
     setTheme('dark-theme-1')
-  } if (page === 'dark.html' && localStorage.getItem('darktheme') === 'dark-theme-2') {
+  } if (page === '/dark.html' && localStorage.getItem('darktheme') === 'dark-theme-2') {
     setTheme('dark-theme-2')
   }
 })();
 
 button.addEventListener('click', () => {
   if (page === '/index.html') {
+    toggleTheme()
+  } if (page === '/') {
     toggleTheme()
   } else {
     toggleDarkTheme()
